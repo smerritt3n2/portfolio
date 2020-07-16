20fedbb4958d63f7458a525ce0ffac24f1322996
smerritt3nr2 <smerritt3nr2@yahoo.com>
2020-07-16 08:54:05 +0100

    1st Upload of Web-project

---
+++ js/sidebar.js
@@ -0,0 +1,7 @@
+function openSlideMenu(){
+    document.getElementById('side-menu').style.width = '250px'  /* Adds width element to the side-menu variable in SCSS */
+}
+
+function closeSlideMenu(){
+    document.getElementById('side-menu').style.width = '0'; /* Changes width element to the side-menu variable in SCSS */
+}
